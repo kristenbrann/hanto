a2ed14ad36ebcadd8f9839f20e655581ba4a0138
Ted Armstrong <theodorecarmstrong@gmail.com>
2014-09-11 10:48:58 -0400

    Alpha Test Package

---
+++ test/test/alpha/testAlphaGame.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package test.alpha;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Nucleus
+ *
+ */
+public class testAlphaGame {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
