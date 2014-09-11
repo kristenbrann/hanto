f91bc0d2a740690bcf9786a74be69294f664da63
Ted Armstrong <theodorecarmstrong@gmail.com>
2014-09-11 12:31:37 -0400

    Test cases for Alpha

--- test/test/alpha/testAlphaGame.java
+++ test/test/alpha/testAlphaGame.java
@@ -4,6 +4,12 @@
 package test.alpha;
 
 import static org.junit.Assert.*;
+import hanto.HantoGameFactory;
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoGame;
+import hanto.common.HantoGameID;
+import hanto.common.HantoPiece;
+import hanto.common.HantoPlayerColor;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,17 +19,36 @@ import org.junit.Test;
  *
  */
 public class testAlphaGame {
+	
+	
+	HantoGame theGame;
+	HantoGameFactory theFactory;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		theFactory = HantoGameFactory.getInstance();
+		theGame = theFactory.makeHantoGame(HantoGameID.ALPHA_HANTO);
+		HantoCoordinate home = new HantoCoordinateImpl(0, 0);
+	}
+	
+	@Test
+	public void gameExists() {
+		assertNotNull(theGame);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNothingOnTheBoard() {
+
+		assertNull(theGame.getPieceAt(home));
+	}
+	
+	@Test
+	public void makeValidFirstMove() {
+		HantoPiece firstPiece = new Butterfly(HantoPlayerColor.BLUE);
+		theGame.makeMove(firstPiece.getType(), null, home);
 	}
 
 }
