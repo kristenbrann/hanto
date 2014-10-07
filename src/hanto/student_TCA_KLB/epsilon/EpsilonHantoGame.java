5e0f368bf3df35ace94684b2f8a519b38923e8c7
kristenbrann <tellkristenstuff@gmail.com>
2014-10-07 14:47:15 -0400

    added files needed for epsilon hanto. no changes within files.

---
+++ src/hanto/student_TCA_KLB/epsilon/EpsilonHantoGame.java
@@ -0,0 +1,32 @@
+package hanto.student_TCA_KLB.epsilon;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+import hanto.student_TCA_KLB.common.AbsHantoGame;
+
+public class EpsilonHantoGame extends AbsHantoGame {
+
+	protected EpsilonHantoGame(HantoPlayerColor color) {
+		super(color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void validateMove(HantoPieceType pieceType, HantoCoordinate from,
+			HantoCoordinate to) throws HantoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected MoveResult determineMoveResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
---
+++ test/test/hanto/student_TCA_KLB/epsilon/testEpsilonGame.java
@@ -0,0 +1,5 @@
+package test.hanto.student_TCA_KLB.epsilon;
+
+public class testEpsilonGame {
+
+}
